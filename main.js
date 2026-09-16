88e28dbb344ae7b744938e25edf8807c82e86b6b
sankalpshuklacmo-design <sankalp.shuklacmo@gmail.com>
2026-09-16 14:42:27 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 const products = [
-  { id: 1, name: "California Badam / Almonds 250g", cat: "nuts", price: 249, mrp: 299, off: "17%", img: "images/mix.jpg" },
-  { id: 2, name: "Premium Kaju W320 250g", cat: "nuts", price: 289, mrp: 349, off: "17%", img: "images/kaju.jpg" },
-  { id: 3, name: "Akbari Pista Roasted Salted 200g", cat: "nuts", price: 399, mrp: 449, off: "11%", img: "images/dryfruits.jpg" },
-  { id: 4, name: "Akhrot Giri / Walnut Kernels 250g", cat: "nuts", price: 369, mrp: 429, off: "14%", img: "images/mix.jpg" },
-  { id: 5, name: "Kishmish Seedless 250g", cat: "dates", price: 89, mrp: 119, off: "25%", img: "images/mix.jpg" },
-  { id: 6, name: "Afghani Anjeer 200g", cat: "dates", price: 279, mrp: 329, off: "15%", img: "images/mix.jpg" },
-  { id: 7, name: "Kimia Dates 500g", cat: "dates", price: 199, mrp: 249, off: "20%", img: "images/mix.jpg" },
-  { id: 8, name: "Panchmeva Mix 500g", cat: "combo", price: 449, mrp: 549, off: "18%", img: "images/mix.jpg" },
-  { id: 9, name: "Roasted Makhana 100g", cat: "snack", price: 149, mrp: 189, off: "21%", img: "images/kaju.jpg" },
-  { id: 10, name: "Chia + Flax Seeds 200g", cat: "seeds", price: 159, mrp: 199, off: "20%", img: "images/mix.jpg" },
-  { id: 11, name: "Haldi Powder 200g", cat: "spice", price: 55, mrp: 75, off: "27%", img: "images/haldi.jpg" },
-  { id: 12, name: "Lal Mirch Powder 200g", cat: "spice", price: 65, mrp: 85, off: "24%", img: "images/masala.jpg" },
-  { id: 13, name: "Jeera Whole 200g", cat: "spice", price: 79, mrp: 99, off: "20%", img: "images/spices.jpg" },
-  { id: 14, name: "Garam Masala 100g", cat: "spice", price: 69, mrp: 89, off: "22%", img: "images/spices.jpg" },
-  { id: 15, name: "Atta 5kg packed", cat: "grocery", price: 245, mrp: 275, off: "11%", img: "images/kaju.jpg" },
-  { id: 16, name: "Basmati Rice 1kg", cat: "grocery", price: 129, mrp: 159, off: "19%", img: "images/kaju.jpg" }
+  { id: 1, name: "California Badam / Almonds 250g", cat: "nuts", price: 249, mrp: 299, off: "17%", img: "mix.jpg" },
+  { id: 2, name: "Premium Kaju W320 250g", cat: "nuts", price: 289, mrp: 349, off: "17%", img: "kaju.jpg" },
+  { id: 3, name: "Akbari Pista Roasted Salted 200g", cat: "nuts", price: 399, mrp: 449, off: "11%", img: "dryfruits.jpg" },
+  { id: 4, name: "Akhrot Giri / Walnut Kernels 250g", cat: "nuts", price: 369, mrp: 429, off: "14%", img: "mix.jpg" },
+  { id: 5, name: "Kishmish Seedless 250g", cat: "dates", price: 89, mrp: 119, off: "25%", img: "mix.jpg" },
+  { id: 6, name: "Afghani Anjeer 200g", cat: "dates", price: 279, mrp: 329, off: "15%", img: "mix.jpg" },
+  { id: 7, name: "Kimia Dates 500g", cat: "dates", price: 199, mrp: 249, off: "20%", img: "mix.jpg" },
+  { id: 8, name: "Panchmeva Mix 500g", cat: "combo", price: 449, mrp: 549, off: "18%", img: "mix.jpg" },
+  { id: 9, name: "Roasted Makhana 100g", cat: "snack", price: 149, mrp: 189, off: "21%", img: "kaju.jpg" },
+  { id: 10, name: "Chia + Flax Seeds 200g", cat: "seeds", price: 159, mrp: 199, off: "20%", img: "mix.jpg" },
+  { id: 11, name: "Haldi Powder 200g", cat: "spice", price: 55, mrp: 75, off: "27%", img: "haldi.jpg" },
+  { id: 12, name: "Lal Mirch Powder 200g", cat: "spice", price: 65, mrp: 85, off: "24%", img: "masala.jpg" },
+  { id: 13, name: "Jeera Whole 200g", cat: "spice", price: 79, mrp: 99, off: "20%", img: "spices.jpg" },
+  { id: 14, name: "Garam Masala 100g", cat: "spice", price: 69, mrp: 89, off: "22%", img: "spices.jpg" },
+  { id: 15, name: "Atta 5kg packed", cat: "grocery", price: 245, mrp: 275, off: "11%", img: "kaju.jpg" },
+  { id: 16, name: "Basmati Rice 1kg", cat: "grocery", price: 129, mrp: 159, off: "19%", img: "kaju.jpg" }
 ];
 
 let filter = "all";
